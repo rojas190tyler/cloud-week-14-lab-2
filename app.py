2d1146000dd341dc8f1013b819df61ec22a76791
rojas190tyler <rojas190@csusm.edu>
2026-05-05 17:00:18 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import random, os
+
+app = Flask(__name__)
+SECRET = random.randint(1, 100)
+
+@app.route('/')
+def root():
+    return jsonify(msg='Guess a number 1-100, try /guess/42')
+
+@app.route('/guess/<int:n>')
+def guess(n):
+    return jsonify(
+        msg='Higher' if n < SECRET else
+            'Lower' if n > SECRET else
+            'Correct!'
+    )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
